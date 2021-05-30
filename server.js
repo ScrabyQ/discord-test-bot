b7feac4af94d40ac648d28721244bf2612a3a9da
ScrabyQ <iameeeboy@gmail.com>
2021-05-30 21:31:23 +0300

    testing

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ app.post('/delete_task', jsonParser, (req, res)=> {
 })
 app.post('/create_task', url_encode, (req, res) => {
 console.log(req.body.deadline)
+res.sendFile('index.html', { root: __dirname})
 res.status(200).end();
 })
 app.get('/create_task', (req, res) => {
