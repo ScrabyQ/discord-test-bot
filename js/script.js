1f347d0ed90f80a64d46f4c043a4800dc5447089
ScrabyQ <iameeeboy@gmail.com>
2021-05-30 14:56:09 +0300

    testing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 'use strict';
-
+let tasks;
 let api = fetch('/get_tasks').then((response) => {
     return response.json()
 }).then((data) => {
-    console.log(data);
+    tasks = data;
 }); 
-console.log(api)
+tasks.forEach(element => {
+    let div = document.createElement('div');
+    div.className = 'col-sm-6';
+    div.innerHTML = `
+<div class="card-header">
+статус задачи
+</div>
+<div class="card-body">
+<h5 class="card-title">${tasks[element].id}</h5>
+<p class="card-text">Описание задачи</p>
+<a href="#" class="btn btn-primary">Редактировать</a>
+</div>
+`
+    document.getElementById('row').append(div)
+});
 
 document.getElementById('listen').addEventListener('click', () => {
     let div = document.createElement('div');
