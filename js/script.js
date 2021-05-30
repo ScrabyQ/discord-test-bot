896ad6d0271e825bac43fc5175bb8f1a573edf5f
ScrabyQ <iameeeboy@gmail.com>
2021-05-30 15:15:22 +0300

    testing

--- js/script.js
+++ js/script.js
@@ -4,23 +4,25 @@ let api = fetch('/get_tasks').then((response) => {
     return response.json()
 }).then((data) => {
     tasks = data;
+}).then(() => {
+    for (element in tasks){
+        console.log(element)
+        let div = document.createElement('div');
+        div.className = 'col-sm-6';
+        div.innerHTML = `
+    <div class="card-header">
+    статус задачи
+    </div>
+    <div class="card-body">
+    <h5 class="card-title">${tasks[element].id}</h5>
+    <p class="card-text">Описание задачи</p>
+    <a href="#" class="btn btn-primary">Редактировать</a>
+    </div>
+    `
+        document.getElementById('row').append(div)
+    };
 }); 
-for (element in tasks){
-    console.log(element)
-    let div = document.createElement('div');
-    div.className = 'col-sm-6';
-    div.innerHTML = `
-<div class="card-header">
-статус задачи
-</div>
-<div class="card-body">
-<h5 class="card-title">${tasks[element].id}</h5>
-<p class="card-text">Описание задачи</p>
-<a href="#" class="btn btn-primary">Редактировать</a>
-</div>
-`
-    document.getElementById('row').append(div)
-};
+
 
 document.getElementById('listen').addEventListener('click', () => {
     let div = document.createElement('div');
