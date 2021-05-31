f3573fe919876dfa5614c3dd2aca0232146d4ce5
ScrabyQ <iameeeboy@gmail.com>
2021-05-31 19:03:50 +0300

    testing

--- js/toggle.js
+++ js/toggle.js
@@ -6,11 +6,33 @@ if (localStorage.getItem('theme') == 'dark'){
     body.setAttribute('style', 'background-image: url(img/backdark.png);');
     body.setAttribute('class', 'body dark')
     document.querySelector('#listen').setAttribute('class', 'text-light')
+
+    // modal works
+    try {
+        document.querySelector('.modal-content').setAttribute('class', 'modal-content bg-dark text-light')
+        document.querySelector('input[name="head"]').setAttribute('class', 'form-control bg-dark text-light')
+        document.querySelector('textarea[name="description"]').setAttribute('class', 'form-control bg-dark text-light')
+        document.querySelector('input[name="deadline"]').setAttribute('class', 'form-control bg-dark text-light')
+        document.querySelector('input[name="responsible"]').setAttribute('class', 'form-control bg-dark text-light')
+    } catch(e){
+        console.log(e)
+    }
 }
 else if (localStorage.getItem('theme') == 'light') {
     body.setAttribute('style', 'background-image: url(img/back.png);');
     body.setAttribute('class', 'body light')
     document.querySelector('#listen').setAttribute('class', 'text-dark')
+
+     // modal works
+     try {
+        document.querySelector('.modal-content').setAttribute('class', 'modal-content bg-light text-dark')
+        document.querySelector('input[name="head"]').setAttribute('class', 'form-control bg-light text-dark')
+        document.querySelector('textarea[name="description"]').setAttribute('class', 'form-control bg-light text-dark')
+        document.querySelector('input[name="deadline"]').setAttribute('class', 'form-control bg-light text-dark')
+        document.querySelector('input[name="responsible"]').setAttribute('class', 'form-control bg-light text-dark')
+    } catch(e){
+        console.log(e)
+    }
 }
 document.querySelector('.toggle-theme').addEventListener('click', () => {
 
