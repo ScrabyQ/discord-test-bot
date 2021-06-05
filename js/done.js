608cc775db74d436a4ca2d4d3002b854d4f7c570
ScrabyQ <iameeeboy@gmail.com>
2021-06-05 16:13:47 +0300

    testing

--- js/done.js
+++ js/done.js
@@ -10,6 +10,28 @@ let api = fetch('/get_done_tasks').then((response) => {
     } else if (localStorage.getItem('theme') === 'dark'){
          setTheme('light', tasks);
     }
+    else {
+        for (let element in tasks){
+            console.log(element)
+            let div = document.createElement('div');
+            div.className = 'col-sm-5 bg-light text-dark';
+            div.setAttribute('style', 'box-shadow: 0 0 10px rgba(0,0,0,0.5); margin-right: 10px;margin-left: 10px; margin-top: 10px; margin-bottom: 10px');
+            div.innerHTML = `
+        <div class="card-header">
+        id задачи: ${tasks[element].id}
+        <button id="${tasks[element].id}" class="btn btn-outline-danger btn-sm delete" style="float: right; margin-bottom: 10px;">X</button>
+        </div>
+        <div class="card-body">
+        <h5 class="card-title">${tasks[element].head}</h5>
+        <p class="card-text">${tasks[element].description}</p>
+        <p class="card-text">${tasks[element].deadline}</p>
+        <p class="card-text">${tasks[element].responsible}</p>
+        <p class="card-text">выполнена: ${tasks[element].done_at.split('T')[0]}</p>
+        </div>
+        `
+            document.getElementById('row').append(div)
+    }
+}
 }).then(() => {
     let delete_buttons = document.querySelectorAll('.delete')
     console.log(typeof(delete_buttons))
