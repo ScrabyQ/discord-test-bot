f3a3198a51c27c79d67702548ce6ca1bd799cab6
ScrabyQ <iameeeboy@gmail.com>
2021-08-15 06:57:18 +0300

    test

--- routes/tasks.route.js
+++ routes/tasks.route.js
@@ -2,7 +2,7 @@ const {Router, static} = require("express");
 const path = require('path')
 const tasks = Router()
 
-router.get(
+tasks.get(
     "/tasks.html",
     static(path.join(__dirname, "/js")),
     (req, res) => {
