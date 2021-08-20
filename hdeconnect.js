6f529a442bd5d06f252f5a94a10bd8be18434e7e
ScrabyQ <iameeeboy@gmail.com>
2021-08-20 09:11:13 +0300

    test

--- hdeconnect.js
+++ hdeconnect.js
@@ -15,10 +15,10 @@ module.exports.getTickets = async function getTickets() {
         }
     }).then(result => {
         if (result.ok){
-            let res = await result.json();
+            let res =  result.json();
           //  module.exports.open_ticketes_count = res.pagination.total;
           console.log("c модуля количество тикетов " + res.pagination.total)
-           return await res.pagination.total
+           return  res.pagination.total
         }
         else {
             console.log('ошибка: ' + result.statusText)
