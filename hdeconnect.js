cabc16feee436ff67ad95481f2d9b38673705221
ScrabyQ <iameeeboy@gmail.com>
2021-08-20 09:17:45 +0300

    test

--- hdeconnect.js
+++ hdeconnect.js
@@ -2,13 +2,11 @@ const config = require('./config.json');
 global.fetch = require('node-fetch');
 const api_key = Buffer.from(config.HDE_TOKEN).toString('base64');
 const domain = 'https://itgt.helpdeskeddy.com/api/v2/'
-let tickets;
-module.exports.tickets = tickets;
 
 
-module.exports.getTickets = function getTickets() {
-    let req_body = `${domain}tickets/?status_list=open`;
-        fetch(req_body, {
+module.exports.getTickets() = function getTickets() {
+    let url = `${domain}tickets/?status_list=open`;
+        fetch(url, {
         method: 'GET',
         headers: {
             'Authorization': 'Basic ' + api_key
