f829758abc992dc4f47a8334c5ce5750816d67ad
ScrabyQ <iameeeboy@gmail.com>
2021-08-20 08:39:05 +0300

    test

--- modules/randomPhrase.js
+++ modules/randomPhrase.js
@@ -1,6 +1,6 @@
 
 
-module.exports.rp = function randomPhrase (...phrases){
+module.exports.rp = function randomPhrase ([phrases]){
     let random = Math.floor(Math.random() * phrases.length)
 
     if (typeof(phrases[random]) == 'string'){
