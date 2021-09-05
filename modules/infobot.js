98e5e26c3a6d82ff0d041d05076fad9589a71bd8
ScrabyQ <iameeeboy@gmail.com>
2021-09-05 09:59:44 +0300

    test

--- modules/infobot.js
+++ modules/infobot.js
@@ -7,6 +7,6 @@ const domain = config.get("infobot.req_domain")
 module.exports.infobot_getBalance = async function getBalance() { 
     let request = await fetch(domain + '/v1/profile/?token=' + token)
     let response = await request.json()
-    console.log(response)
+    //console.log(response)
     return response.data.attributes.balance
 }
